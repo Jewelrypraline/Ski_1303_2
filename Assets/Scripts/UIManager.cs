a2be9162ab36bf55a75b419a3e69e60f6794cc56
Jewelrypraline <meam7462@gmail.com>
2026-08-25 23:21:54 +0700

    Restart,Tree

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text notiText;
+
+    [SerializeField]
+    private GameObject reststartButton;
+
+    [SerializeField]
+    private Player player;
+
+    public static UIManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowNotiText(string s)
+    {
+        notiText.text = s;
+    }
+
+    public void RestartGame()
+    {
+        player.transform.position = new Vector3(0f, 88f, -86.25f);
+    }
+}
